969af8029b398a71550730f8e76c0eb5fa75ae97
203426 <203426@ids.upchiapas.edu.mx>
2022-03-16 10:44:49 -0600

    update to database

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,6 @@
 
 /* Creating database */
-CREATE DATABSE dbCashF
+CREATE DATABSE dbCashFlow
 
 
 
@@ -8,27 +8,41 @@ CREATE DATABSE dbCashF
 /* Creating table for login */
 CREATE TABLE login(
     id SERIAL PRIMARY KEY,
-    user VARCHAR(255) UNIQUE,
-    password VARCHAR(255)
+    username VARCHAR(255) UNIQUE not null,
+    password VARCHAR(255) not null
 );
 
 
 /* Creating table for categorias */
 CREATE TABLE categorias(
     id SERIAL PRIMARY KEY,
-    clasificacion VARCHAR(255),
-    categoria VARCHAR(255),
-    sub_categoria VARCHAR(255)
+    clasificacion VARCHAR(255) not null,
+    categoria VARCHAR(255) not null,
+    sub_categoria VARCHAR(255) not null
 );
 
 /* Creating table for flujo de efectivo */
 CREATE TABLE flujo_efectivo(
     id SERIAL PRIMARY KEY,
-    id_categoria int REFERENCES categorias(id),
-    es_ingreso boolean,
-    descripcion VARCHAR(255),
-    cantidad bigint,
-    fecha date
+    id_categoria int REFERENCES categorias(id) not null,
+    es_ingreso boolean not null,
+    descripcion VARCHAR(255) not null,
+    cantidad bigint not null,
+    fecha date not null
 );
 /* Ejemplo de insertción de datos a la tabla flujo de efectivo */
 insert into flujo_efectivo (id_categoria,es_ingreso,descripcion, cantidad, fecha) values (1, FALSE,'pago de ...', 20000, '15/3/2022' )
+
+
+
+/* Creating table for  indicadores_de_dinero*/
+CREATE TABLE indicadores(
+    id SERIAL PRIMARY KEY,
+    num_sem int not null,
+    mes VARCHAR(255) not null,
+    descrip VARCHAR(255) not null,
+    monto bigint not null
+);
+
+/* Ejemplo de insertción de datos a la tabla indicadores*/
+insert into indicadores (num_sem,mes,descrip,monto) values (1, 'abril', 'pago' , 500)
